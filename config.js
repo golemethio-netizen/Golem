995ed8673ee5f2aed20685807485813de2001b71
John <golemethio@gmail.com>
2026-02-13 15:16:59 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+const supabaseUrl = 'https://ryeylhawdmykbbmnfrrh.supabase.co';
+const supabaseKey = ''sb_publishable__XhkM93G4uNhdKhDKa6osQ_PPpIPO6m'';
+const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
