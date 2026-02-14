1e6b0a159422a67cfcb7c2f19bf11f552a6c3b09
John <golemethio@gmail.com>
2026-02-14 11:43:41 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 // Ensure you have ' ' around the URL and Key!
 const SUPABASE_URL = 'https://ryeylhawdmykbbmnfrrh.supabase.co';
-const SUPABASE_KEY = 'sb_publishable__XhkM93G4uNhdKhDKa6osQ_PPpIPO6m';  // Put quotes here!
+const SUPABASE_KEY = '8557174379:AAHjA_5WAIxIR8uq4mjZOhd1EfdKvgI2s7o';  // Put quotes here!
 
 const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
